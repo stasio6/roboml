1ef86174f91f139c6c462dfe59a7c37189977d1b
stan <stasiek.strzelecki@gmail.com>
2024-06-05 21:59:37 -0700

    SQIL genmore

--- SQIL_genmore.py
+++ SQIL_genmore.py
@@ -77,10 +77,12 @@ def parse_args():
         help="automatic tuning of the entropy coefficient")
     
     parser.add_argument("--output-dir", type=str, default='output')
-    parser.add_argument("--eval-freq", type=int, default=30_000)
+    parser.add_argument("--eval-freq", type=int, default=100_000)
     parser.add_argument("--num-envs", type=int, default=4)
-    parser.add_argument("--num-eval-episodes", type=int, default=10)
-    parser.add_argument("--num-eval-envs", type=int, default=1)
+    parser.add_argument("--num-eval-episodes", type=int, default=200)
+    parser.add_argument("--num-eval-envs", type=int, default=4)
+    parser.add_argument("--gen-more-thres", type=float, default=1.1) # Default is never
+    parser.add_argument("--num-traj-gen-more", type=int, default=1000)
     parser.add_argument("--sync-venv", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True)
     parser.add_argument("--num-steps-per-update", type=float, default=2)
     parser.add_argument("--training-freq", type=int, default=64)
@@ -346,6 +348,48 @@ def evaluate(n, agent, eval_envs, device):
     print('======= Evaluation Ends =========')
     return result
 
+def extend_dataset(dataset, actor, envs, num_envs, traj_to_generate, modif, max_env_len, device):
+    generated = 0
+    buffers = [[] for _ in range(num_envs)]
+    add_actions = []
+    add_obs = []
+    add_next_obs = []
+    add_rewards = []
+    obs = envs.reset()
+    while generated < traj_to_generate:
+        actions = actor.get_eval_action(torch.Tensor(obs).to(device))
+        actions = actions.detach().cpu().numpy()
+
+        next_obs, rewards, dones, infos = envs.step(actions)
+        real_next_obs = next_obs.copy()
+
+        for idx, d in enumerate(dones):
+            if d:
+                real_next_obs[idx] = infos[idx]["terminal_observation"]
+
+        # print(infos)
+        for e in range(num_envs):
+            buffers[e].append((obs[e], real_next_obs[e], actions[e]))
+            if dones[e]:
+                # print(infos[e])
+                if infos[e]['success']:
+                    generated += 1
+                    reward = 1 - modif * len(buffers[e]) / max_env_len
+                    print("Success")
+                    for o, no, a in buffers[e]:
+                        add_obs.append(o)
+                        add_next_obs.append(no)
+                        add_actions.append(a)
+                        add_rewards.append(reward)
+                buffers[e] = []
+        obs = next_obs
+
+    dataset.demo_data['observations'] = torch.cat([dataset.demo_data['observations'], torch.tensor(add_obs)], dim=0).float().to(device)
+    dataset.demo_data['next_observations'] = torch.cat([dataset.demo_data['next_observations'], torch.tensor(add_next_obs)], dim=0).float().to(device)
+    dataset.demo_data['actions'] = torch.cat([dataset.demo_data['actions'], torch.tensor(add_actions)], dim=0).float().to(device)
+    dataset.demo_data['rewards'] = torch.cat([dataset.demo_data['rewards'], torch.tensor(add_rewards)], dim=0).float().to(device)
+    return dataset
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -437,6 +481,7 @@ if __name__ == "__main__":
     # )
     
     dataset = SmallDemoDataset(envs, args.demo_path, envs.single_observation_space, 'cpu', args.buffer_size, args.num_envs, device, args.ep_len_reward_weight, make_env(args.env_id, 0)().spec.max_episode_steps, num_traj=args.num_demo_traj)
+    did_extend_dataset = False
 
     # TRY NOT TO MODIFY: start the game
     start_time = time.time()
@@ -572,6 +617,12 @@ if __name__ == "__main__":
             eval_time += time.time() - tic
             for k, v in result.items():
                 writer.add_scalar(f"eval/{k}", np.mean(v), global_step)
+                if k == 'success':
+                    sr = np.mean(v)
+            print("Success rate:", sr)
+            if sr > args.gen_more_thres and not did_extend_dataset:
+                dataset = extend_dataset(dataset, actor, eval_envs, args.num_eval_envs, args.num_traj_gen_more, args.ep_len_reward_weight, make_env(args.env_id, 0)().spec.max_episode_steps, device)
+                did_extend_dataset = True
         
         # Checkpoint
         if args.save_freq and ( global_step >= args.total_timesteps or \
